a6fd294eff20fe93ec9c920f6b91addf1488572c
Link Chree <chiiicoohlink@gmail.com>
2021-12-17 17:50:44 +0800

    fixing touch style

--- envjs/env.js
+++ envjs/env.js
@@ -34,11 +34,11 @@ css(".stripe-background", { backgroundImage: `linear-gradient(-45deg, #ffffff33 
 css(".stripe-background-2", { backgroundImage: `linear-gradient(45deg, #ffffff33 25%, #00000000 25%, #00000000 50%, #ffffff33 50%, #ffffff33 75%, #00000000 75%, #00000000)`, backgroundSize: "20px 20px" })
 css(".resizer:hover", { background: "#00000066" })
 css(".resizer", { transition: "background 0.3s", position: "absolute" })
-css(".noflow", { position: "relative", overflow: "hidden" })
+css(".noflow", { position: "relative", overflow: "hidden", display: "flex" })
 
-$.warp = dom({ class: "noflow", style: { height: "100%", display: "flex" } }, shadow)
+$.warp = dom({ class: "noflow", style: { height: "100%", touchAction: "none" } }, shadow)
 $.edtdiv = repleditor(data), $.execdiv = dom({ class: "stripe-background noflow" }, warp)
-style(execdiv, { display: "flex", flexDirection: "column" })
+style(execdiv, { flexDirection: "column" })
 $.appdiv = dom({}, execdiv), $.sdbxdiv = dom({}, appdiv)
 $.dbgdiv = dom({ class: "stripe-background-2" }, execdiv)
 style(appdiv, { height: "50%", position: "relative", overflow: "hidden" })
@@ -55,32 +55,36 @@ $.setusersize = us => ($.usersize = us, resize()), $.resize = () => {
   style(sdbxdiv, { transformOrigin: "left top", position: "absolute" })
 }; new ResizeObserver(resize).observe(appdiv)
 
-const resizebar_width = 18, rbw = resizebar_width, pct = s => s * 100 + "%"
+const resizebar_width = 17.5, rbw = resizebar_width, pct = s => s * 100 + "%"
 const noselect = w => w.getSelection().removeAllRanges()
 const toclose = (s, i, a = Infinity) => (s === 0 || s === 1 ? 0.5 : (forof([0, 0.5, 1]
   .map(v => [v, Math.abs(s - v)]), ([v, c]) => a > c ? (a = c, i = v) : 0), i))
 
 $.edtrb = dom({ class: "resizer" }), edtdiv.after(edtrb)
 style(edtrb, { width: rbw, height: "100%", cursor: "col-resize", zIndex: "98" })
-$.rszedt = (s = edtscale, left = `calc(${pct(s)} - ${rbw / 2}px)`) => (
-  $.edtscale = s, style(edtdiv, { width: pct(s) }), style(allrb, { left }),
-  style(edtrb, { left }), style(execdiv, { width: pct(1 - s) }))
+$.rszedt = (s = edtscale, l = (e = 2) => `calc(${pct(s)} - ${rbw / e}px)`) => (
+  $.edtscale = s, style(edtdiv, { width: pct(s) }), style(allrb, { left: l(2 / ars) }),
+  style(edtrb, { left: l() }), style(execdiv, { width: pct(1 - s) }))
 edtrb.onpointerdown = adddrag(edtrb, (e, w) => (noselect(w), rszedt(mn(mx(e.x, 0) / w.innerWidth, 1))))
 edtrb.ondblclick = () => rszedt(edtscale === 0.5 ? 0 : toclose(edtscale))
 
 $.exerb = dom({ class: "resizer" }), appdiv.after(exerb)
 style(exerb, { width: "100%", height: rbw, cursor: "row-resize", zIndex: "97" })
-$.rszexe = (s = exescale, top = `calc(${pct(s)} - ${rbw / 2}px)`) => (
-  $.exescale = s, style(appdiv, { height: pct(s) }), style(allrb, { top }),
-  style(exerb, { top }), style(dbgdiv, { height: pct(1 - s) }))
+$.rszexe = (s = exescale, t = (e = 2) => `calc(${pct(s)} - ${rbw / e}px)`) => (
+  $.exescale = s, style(appdiv, { height: pct(s) }), style(allrb, { top: t(2 / ars) }),
+  style(exerb, { top: t() }), style(dbgdiv, { height: pct(1 - s) }))
 exerb.onpointerdown = adddrag(exerb, (e, w) => (noselect(w), rszexe(mn(mx(e.y, 0) / w.innerHeight, 1))))
 exerb.ondblclick = () => rszexe(exescale === 0.5 ? 1 : toclose(exescale))
 
-$.allrb = dom({ class: "resizer" }), edtrb.after(allrb)
-style(allrb, { width: rbw, height: rbw, cursor: "row-resize", zIndex: "99" })
+$.allrb = dom({ class: "resizer" }), edtrb.after(allrb), $.ars = 2
+style(allrb, { width: rbw * ars, height: rbw * ars, cursor: "all-scroll", zIndex: "99" })
 allrb.onpointerdown = adddrag(allrb, (e, w) => (noselect(w),
   rszedt(mn(mx(e.x, 0) / w.innerWidth, 1)), rszexe(mn(mx(e.y, 0) / w.innerHeight, 1))))
 allrb.ondblclick = () => (edtrb.ondblclick(), exerb.ondblclick())
+
+const _tstyle = { transition: "all 0.3s ease-in-out 0s" }
+style(edtdiv, _tstyle), style(execdiv, _tstyle)
+style(appdiv, _tstyle), style(dbgdiv, _tstyle)
 
 rszedt(edtscale), rszexe(exescale)
 $.sdbx = await sandbox({ root: sdbxdiv }, proto($))
