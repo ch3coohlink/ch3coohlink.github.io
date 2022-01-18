c785f68972849e3341a8b0cfe173597a1aeb58f2
Link Chree <chiiicoohlink@gmail.com>
2022-01-19 01:24:24 +0800

    that's all you need

--- envjs/js/envjs.js
+++ envjs/js/envjs.js
@@ -1,9 +1,25 @@
 const body = document.body; body.innerHTML = ""
 const store = idb(assign(scope($), { name: "envjs" }))
+css("body", { margin: 0 })
 
-const editor = () => {
-
+const editor = d => {
+  let sdbx, r = dom({ style: { display: "flex" } })
+  const onkeydown = e => e.key === "s" && e.ctrlKey ? (e.preventDefault(), upd())
+    : e.key === "n" && e.altKey ? (e.preventDefault(), splice(d.i + 1, 0, { v: "" }))
+      : e.key === "w" && e.altKey ? (e.preventDefault(), splice(d.i, 1)) : 0
+  const upd = () => (d.v = t.value, save(), refresh())
+  const t = dom({ tag: "textarea", value: d.v, onkeydown, spellcheck: false })
+  style(t, { resize: "vertical", width: "50%" })
+  const refresh = () => (sdbx?.destroy(), sdbx = sandbox(scope($)),
+    r = elm(r, { child: [t, sdbx.document.documentElement], diff: true }),
+    Function("$", `with($){\n${d.v}\n}`)(sdbx))
+  setTimeout(refresh); d.d = r; return d
 }
-const editors = () => {
 
-}
+const gsl = "global-state-location"
+$.save = () => store.set(gsl, data.map(({ v }) => ({ v })))
+$.sync = () => elm(editors, { child: data.map(({ d }) => d), diff: true })
+$.splice = (s, c, ...a) => (data.splice(s, c, ...a.map(editor)),
+  forrg(data.length, i => data[i].i = i), sync())
+store.get(gsl).then(v => (body.append($.editors = dom()), $.data = [],
+  (v ??= []).length > 0 ? 0 : v.push({ v: "" }), splice(0, 0, ...v)))
--- envjs/js/sandbox.js
+++ envjs/js/sandbox.js
@@ -1,22 +1,13 @@
 // This is by no mean a secure sandbox.
-$.fillup ??= true, $.useshadow ??= true
-$.initstyle = true, $.nomargin ??= false
-$.stdlib ??= true, $.extralib ??= true
-$.extra ??= {}
+$.stdlib ??= true, $.extralib ??= true, $.extra ??= {}
 
 // dom element
-!useshadow ? $.shadow = $.warp = root :
-  $.shadow = ($.warp = dom({}, root)).attachShadow({ mode: "open" })
-$.html = dom({ lang: "en" }, shadow, "html")
+$.html = dom({ tag: "html", lang: "en" })
 $.head = dom({}, html, "head"), $.body = dom({}, html, "body")
-$.css = useshadow ? createcss(dom({}, head, "style")) : css
-fillup ? (style(html, { width: "100%", position: "absolute" }),
-  style(warp, { height: "100%", width: "100%", position: "relative", overflow: "auto" })) : 0
-nomargin ? style(body, { margin: 0 }) : 0
 
-// css is callable only after style tag is in dom
-const init = () => { initstyle ? css("html", { all: "initial" }) : 0 }
-try { init() } catch (e) { }
+// clear everything inside sandbox
+const destroy = () => (forof(clr, f => f()),
+  forin(ds, (v, k) => or(k, v)), ro?.disconnect())
 
 // everything that not gc should be concerned
 // (so maybe add URL.createObjectURL etc...)
@@ -25,12 +16,13 @@ const pack = (n, [a, b] = n.split(" "), m = new WeakMap, d = new Set
   $[a] = (f, t) => (i => (rg.register(f, i), m.has(f)
     ? d.delete(m.get(f)) : 0, m.set(f, i), d.add(i), i))(r(f, t)),
   $[b] = i => (c(i), d.delete(i)), () => forof(d, $[b]))
-const clr = [pack("requestIdleCallback cancelIdleCallback"), pack("setTimeout clearTimeout")
-  , pack("requestAnimationFrame cancelAnimationFrame"), pack("setInterval clearInterval")]
+const clr = [pack("requestIdleCallback cancelIdleCallback")
+  , pack("requestAnimationFrame cancelAnimationFrame"), pack("setNow clearNow")
+  , pack("setTimeout clearTimeout"), pack("setInterval clearInterval")]
 
 // dispatch all window event
 const en = `search appinstalled beforeinstallprompt beforexrselect abort blur cancel canplay canplaythrough change click close contextmenu cuechange dblclick drag dragend dragenter dragleave dragover dragstart drop durationchange emptied ended error focus formdata input invalid keydown keypress keyup load loadeddata loadedmetadata loadstart mousedown mouseenter mouseleave mousemove mouseout mouseover mouseup mousewheel pause play playing progress ratechange reset resize scroll seeked seeking select stalled submit suspend timeupdate toggle volumechange waiting webkitanimationend webkitanimationiteration webkitanimationstart webkittransitionend wheel auxclick gotpointercapture lostpointercapture pointerdown pointermove pointerup pointercancel pointerover pointerout pointerenter pointerleave selectstart selectionchange animationend animationiteration animationstart transitionrun transitionstart transitionend transitioncancel afterprint beforeprint beforeunload hashchange languagechange message messageerror offline online pagehide pageshow popstate rejectionhandled storage unhandledrejection unload devicemotion deviceorientation deviceorientationabsolute pointerrawupdate`.split(" ")
-const ls = {}, ds = {}, d = t => e => (w["on" + t]?.(e), forof(ls[t] ?? [], f => f(e)))
+let ls = {}, ds = {}, d = t => e => (w["on" + t]?.(e), forof(ls[t] ?? [], f => f(e)))
 const oa = addEventListener, or = removeEventListener; forof(en, k => ds[k] = null)
 const initevent = t => ls[t] ??= (t === "resize" ? wr(d(t)) : oa(t, ds[t] = d(t)), new Set)
 const oneventset = (t, v) => isfct(v) && t in ds ? initevent(t) : 0
@@ -38,12 +30,8 @@ $.addEventListener = (t, l) => initevent(t).add(l)
 $.removeEventListener = (t, l) => ls[t]?.delete(l)
 
 // watch resize of root element
-let ro, wr = f => (ro = new ResizeObserver(e => f(tr(e)))).observe(root)
+let ro, wr = f => (ro = new ResizeObserver(e => f(tr(e)))).observe(html)
 const tr = (entries) => entries // TODO
-
-// clear everything inside sandbox
-const destroy = () => (warp.remove(), forof(clr, f => f()),
-  forin(ds, (v, k) => or(k, v)), ro?.disconnect())
 
 // warp a window interface
 const set = (t, k, v) => (k.startsWith("on") ? oneventset(k.slice(2), v) : 0, t[k] = v, 1)
@@ -52,10 +40,10 @@ forof("$ window globalThis self".split(" "), n => property(w, n, { value: _ }))
 
 // document and other warp function
 const get = (t, k) => k in t ? t[k] : document[k]
-const _d = { documentElement: html, head, body, warp }
-assign(w, { document: new Proxy(_d, { get }), init, destroy, css })
+const _d = { documentElement: html, head, body }
+assign(w, { document: new Proxy(_d, { get }), destroy, css })
 assign(w, { setInterval, clearInterval, setTimeout, clearTimeout })
-assign(w, { requestAnimationFrame, cancelAnimationFrame })
+assign(w, { requestAnimationFrame, cancelAnimationFrame, setNow, clearNow })
 assign(w, { requestIdleCallback, cancelIdleCallback })
 assign(w, { addEventListener, removeEventListener })
 
@@ -65,7 +53,7 @@ stdlib ? (assign(w, { isnum, isstr, isudf, isobj, isfct, isbgi }),
   assign(w, { forrg, maprg, forin, forof, cases, panic }),
   assign(w, { proto, property, assign, deletep, create, extract, exclude, scope }),
   assign(w, { log, clear, dom, elm, style, createcss, dsplice, swaptag })) : 0
- 
+
 // extra function
 if (extralib) {
   // extension function: schedule
