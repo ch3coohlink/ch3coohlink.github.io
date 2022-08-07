10155911121597a561f8eb9db489fadf5deffd42
Link Chree <chiiicoohlink@gmail.com>
2022-08-07 16:45:42 +0800

    add new chapter for music soft

--- ltjs/server.js
+++ ltjs/server.js
@@ -1,49 +1,23 @@
-const fs = require("fs")
-const ws = require("ws")
-const http = require("http")
-const path = require("path")
+const fs = require("fs"), http = require("http")
+const ws = require("ws"), path = require("path")
+const url = require("url")
 
-http.createServer((rq, rs) => {
-  const filePath = "." + rq.url
-  const contentType = {
-    ".html": "text/html",
-    ".js": "text/javascript",
-    ".css": "text/css",
-    ".json": "application/json",
-    ".png": "image/png",
-    ".jpg": "image/jpg",
-    ".wav": "audio/wav",
-  }[path.extname(filePath)] ?? "text/plain"
+http.createServer((rq, rs,
+  filePath = "." + url.parse(rq.url, true).pathname, contentType = {
+    ".html": "text/html", ".js": "text/javascript",
+    ".css": "text/css", ".json": "application/json",
+    ".png": "image/png", ".jpg": "image/jpg", ".wav": "audio/wav",
+  }[path.extname(filePath)] ?? "text/plain") =>
   fs.readFile(filePath, function (error, content) {
-    if (error) {
-      if (error.code == "ENOENT") {
-        fs.readFile("./404.html", function (_, content) {
-          rs.writeHead(200, { "Content-Type": contentType })
-          rs.end(content, "utf-8")
-        })
-      }
-      else {
-        rs.writeHead(500)
-        rs.end("Sorry, check with the site admin for error: " + error.code + " ..\n")
-        rs.end()
-      }
-    }
-    else {
-      rs.writeHead(200, { "Content-Type": contentType })
-      rs.end(content, "utf-8")
-    }
-  })
-}).listen(8080)
+    if (error) rs.writeHead(500), rs.end(error.code); else (rs.writeHead
+      (200, { "Content-Type": contentType }), rs.end(content, "utf-8"))
+  })).listen(8080)
 
-const { log: _lg } = console
-const log = (v, ...a) => (_lg(v, ...a), v)
-const debounce = (f, d = 100, c = true, cf = () => c = true) =>
+const debounce = (f, d = 1000, c = true, cf = () => c = true) =>
   (...a) => c ? (c = false, setTimeout(cf, d), f(...a)) : undefined
-const update = debounce(() => {
-
-  fs.readFile("./ltjs/index.dat", "utf8", (_, data) => {
-
-    let state = "html", parts = [], $ = { ws }
+const _ = {}, update = debounce(() =>
+  fs.readFile("./ltjs/index.dat", "utf8", (e, data) => {
+    let state = "html", parts = [], $ = { ws, _ }
     for (const line of data.split(/\r?\n/)) {
       if (line.startsWith("###")) {
         state = line.slice(3).trim()
@@ -53,35 +27,13 @@ const update = debounce(() => {
         if (parts.length === 0) parts.push({ state, data: [] })
         parts[parts.length - 1].data.push(line)
       }
-    }
-
-    for (let { state, data } of parts) {
-      data = data.join("\n")
-
-      switch (state) {
-        case "server": {
-          try { new Function(`with($){\n${data}\n}`)() }
-          catch (e) { console.error(data + "\n", e) }
-        } break; default: break
+    } for (let { state, data } of parts) {
+      data = data.join("\n"); switch (state) {
+        case "server": new Function("$", `with($){\n${data}\n}`)($);
+          break; default: break
       }
     }
-  })
-
-})
+  }))
 
 update(), fs.watch("./ltjs", update)
-
-
-// const wss = new ws.Server({ port: 8080 })
-// wss.on("connection", ws => {
-//   log("new client connected")
-//   ws.on("message", data => {
-//     log(`Client has sent us: ${data}`)
-//   })
-//   ws.on("close", () => {
-//     log("the client has connected");
-//   });
-//   ws.onerror = function () {
-//     log("Some Error occurred")
-//   }
-// })
+process.on('uncaughtException', e => (console.error(e), update()))
