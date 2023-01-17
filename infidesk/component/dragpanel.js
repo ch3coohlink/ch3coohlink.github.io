e5ad41b05b773171dd1fb36a7aa14982cf30c244
Link Chree <chiiicoohlink@gmail.com>
2023-01-17 22:24:25 +0800

    I hate HTML...

--- infidesk/component/dragpanel.js
+++ infidesk/component/dragpanel.js
@@ -3,10 +3,22 @@ $.panel = dom({ class: "full" }, bg)
 
 compappend(bg, complist({
   direction: "vertical-reverse",
-  child: [$.scalebt = button({ text: "⇱" })],
+  child: [$.scalebt = button()],
   class: "float-button-list",
   specsty: `.list { width: 90px; }`,
 }))
+
+$.fs = false
+const dummy = dom()
+const setfs = (b = fs) => {
+  $.fs = b
+  scalebt.text = b ? "⇲" : "⇱"
+  b ? relm.replaceWith(dummy) : dummy.replaceWith(relm)
+  if (b) { fsdiv.append(relm), relm.classList.add("full") }
+  else { relm.classList.remove("full") }
+}
+scalebt.on("click", () => setfs(!fs))
+setfs()
 
 $.atarget = panel; const oa = append
 $.append = (...a) => oa(...a.map(e => e))
--- infidesk/index.js
+++ infidesk/index.js
@@ -2,6 +2,7 @@ await loadsym("./basic.js")
 
 const cssst = new CSSStyleSheet()
 gettext("./common.css").then(t => cssst.replace(t))
+document.adoptedStyleSheets.push(cssst)
 
 $.compbase = await require("./compbase.js")
 $.newcomp = async name => {
@@ -24,8 +25,8 @@ await Promise.all(list.map(n => newcomp(n).then(f => $[n] = f)))
 $.newidb = await require("./idb.js")
 $.idb = newidb($, { name: "infidesk" })
 $.save = idb.saveobj("lmm6keutnfvlhnuj08vq1nu1mhsfjrtm")
-
 await save.init
+
 const setscroll = (i = 0) => {
   window.scrollTo(0, save.scrollY ??= 0)
   if (window.scrollY !== save.scrollY && i < 10)
@@ -36,29 +37,10 @@ const body = document.body
 compappend(body, $.stexp = showcase())
 addEventListener("scroll", () => save.scrollY = window.scrollY)
 
-stexp.append("basic component: text editor",
-  texteditor({ label: "test-label" }))
-stexp.append("basic component: button",
-  dom({
-    class: "grid-bg",
-    style: { padding: 5 },
-    child: [button()
-      , button({ text: "text too long ".repeat(10) })]
-  }))
-stexp.append("basic component: component list",
-  dom({
-    class: "grid-bg",
-    style: { padding: 5 },
-    child: [complist({
-      direction: "horizontal-reverse", child: [
-        texteditor({
-          label: "special text editor with background",
-          value: "this text editor has white background ".repeat(10)
-        }),
-        ..."↻◰⇱⊞⊠⩩".split("").map(n => button({ text: n }))],
-      specsty: `.list>* { width: 70px; }
-        .texteditor { background: white; width: 200px; }`
-    })]
-  }))
+style($.fsdiv = dom({}, body),
+  { position: "fixed", width: "100vw", height: "100vh" },
+  { top: 0, left: 0, pointerEvents: "none", zIndex: "1000" }
+)
+
 stexp.append("basic component: drag panel",
-  style(dragpanel(), { height: 500 }))
+  dragpanel())
