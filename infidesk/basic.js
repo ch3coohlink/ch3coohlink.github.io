275046ae66fee4bfc1e5928d351b597542e2e254
Link Chree <chiiicoohlink@gmail.com>
2023-01-18 19:03:09 +0800

    some prng

--- infidesk/basic.js
+++ infidesk/basic.js
@@ -47,3 +47,20 @@ $.eventtarget = $ => {
     eventdict[k].size > 0 ? 0 : delete eventdict[k])
   return $
 }
+
+let { imul } = Math, mb32 = a => t =>
+  (a = a + 1831565813 | 0,
+    t = imul(a ^ a >>> 15, 1 | a),
+    t = t + imul(t ^ t >>> 7, 61 | t) ^ t,
+    (t ^ t >>> 14) >>> 0) / 4294967296
+
+let sfc32 = (a, b, c, d) => () => {
+  a |= 0; b |= 0; c |= 0; d |= 0;
+  let t = (a + b | 0) + d | 0;
+  d = d + 1 | 0;
+  a = b ^ b >>> 9;
+  b = c + (c << 3) | 0;
+  c = c << 21 | c >>> 11;
+  c = c + t | 0;
+  return (t >>> 0) / 4294967296;
+}
