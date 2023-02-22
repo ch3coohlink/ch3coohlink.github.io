2ec43b28096444336b1193b0a288c7e734ce5526
Link Chree <chiiicoohlink@gmail.com>
2023-02-22 19:59:15 +0800

    backup code

--- infidesk/index.js
+++ infidesk/index.js
@@ -23,6 +23,7 @@ $.box = (...a) => {
     o.size = () => {
       let l = o.layout, w = l.width ?? 300, h = 5
       for (let i of child) {
+        if (i.layout.position) { continue }
         let [dw, dh] = i.size()
         w = max(w, dw + 10), h += dh + 5
       } o.temp.w = w, h = o.temp.h = max(l.height ?? 100, h)
@@ -31,26 +32,66 @@ $.box = (...a) => {
     o.pos = () => {
       let { x, y, w } = temp, lx = 5, ly = 5
       for (let i of child) {
-        let pw = w - 10, ix, iy; if (pw > i.temp.w) { i.temp.w = pw }
+        let ix, iy
         if (i.layout.position) { [ix, iy] = i.layout.position }
         else {
           ix = x + lx, iy = y + ly
           ly += i.temp.h + 5
-        } i.temp.x = ix, i.temp.y = iy, i.pos()
+        }
+        i.temp.x = ix, i.temp.y = iy
+        i.pos(), i.grow(o)
       }
+    }
+    o.grow = (p) => {
+      // let pw = w - 10; if (pw > i.temp.w) { i.temp.w = pw }
+      let pw = w - i.temp.x - 5; if (pw > i.temp.w) { i.temp.w = pw }
     }
     o.draw = () => {
       ctx.fillStyle = "#00000050"
-      ctx.fillRect(temp.x, temp.y, temp.w, temp.h)
+      let m = new DOMMatrix().translate(temp.x, temp.y)
+      ctx.setTransform(m)
+      ctx.fillRect(0, 0, temp.w, temp.h)
       for (let i of child) { i.draw() }
       o.temp = {}
     }
   } return o
 }
 
-$.button = () => { }
+$.text = () => {
+  let o = {}; with (o) {
+    o.type = "text"
+    o.child = []
+    o.temp = {}
+    o.layout = {}
+    o.size = () => { }
+    o.pos = () => { }
+    o.grow = () => { }
+    o.draw = () => { }
+  } return o
+}
 
-$.root = []
+$.root = () => {
+  let o = {}; with (o) {
+    o.type = "root"
+    o.child = []
+    o.size = () => { for (let i of child) { i.size() } }
+    o.pos = () => {
+      let lx = 5, ly = 5, x = 0, y = 0, w = cvs.width
+      for (let i of child) {
+        let ix, iy
+        if (i.layout.position) { [ix, iy] = i.layout.position }
+        else {
+          ix = x + lx, iy = y + ly
+          ly += i.temp.h + 5
+        } i.temp.x = ix, i.temp.y = iy
+
+        let pw = w - i.temp.x - 5; if (pw > i.temp.w) { i.temp.w = pw }
+        i.pos()
+      }
+    }
+    o.draw = () => { for (let i of child) { i.draw() } o.temp = {} }
+  } return o
+}
 frame(t => {
   t /= 1000
 
@@ -61,28 +102,10 @@ frame(t => {
 
   ctx.fillStyle = "white"
   ctx.fillRect(0, 0, cvs.width, cvs.height)
-
-  for (let i of root) { i.size() }
-  let lx = 5, ly = 5, x = 0, y = 0, w = cvs.width
-  for (let i of root) {
-    let ix, iy
-    if (i.layout.position) { [ix, iy] = i.layout.position }
-    else {
-      ix = x + lx, iy = y + ly
-      ly += i.temp.h + 5
-    } i.temp.x = ix, i.temp.y = iy
-
-    let pw = w - i.temp.x - 5; if (pw > i.temp.w) { i.temp.w = pw }
-    i.pos()
-  }
-  for (let i of root) { i.draw() }
-
-  ctx.fillStyle = "black"
-  ctx.font = "25px consolas"
-  ctx.fillText("OK...", 100, 200)
-  ctx.fillText(pnow().toFixed(1), 100, 230)
+  rt.size(), rt.pos(), rt.draw()
 })
 
+$.rt = root()
 $.panel = box(
   box(),
   box(box(box(), box())),
@@ -90,4 +113,4 @@ $.panel = box(
   box(),
 )
 panel.layout.height = 800
-root.push(panel, box())
+rt.child.push(panel, box())
