121e4eaa59361e9da4cb325806880abe2787e8a4
ch3coohlink <chiiicoohlink@gmail.com>
2023-08-24 19:48:08 +0800

    refactoring

--- infidesk/newdev/index.js
+++ infidesk/newdev/index.js
@@ -117,152 +117,187 @@ $.gt = git(db)
 // TODO: node description
 // TODO: mobile UI support
 
-window.addEventListener("beforeunload", (e) => {
-  if (current_file_changed) {
-    check_file_save_dialog()
-    return e.returnValue = "file has been changed"
-  }
-})
-window.addEventListener("load", async () => {
-  // 当前文件/代码库/版本
-  $.save = db.saveobj("6s80s1u052ftdj009g2fd7brflbv1iq8")
-  await save.init
-
-  // 单个input流程
-  $.single_input_message = f => async () => {
-    openmessage()
-    const ctn = dom({ class: "window" }, messagectn)
-    const ipt = dom({ tag: "input" }, ctn)
-    const btn = dom({ tag: "button", child: "Enter" }, ctn)
-    const apply = commit_dialog(f)
-    ipt.addEventListener("keydown", e => e.key === "Enter" ? apply(ipt) : 0)
-    btn.addEventListener("click", () => apply(ipt))
-  }
-  // 新建一个repo，当没有处于一个版本的时候，切换到这个新repo的根节点
-  $.new_repo = async (name) => {
-    if (!name) { panic(`invalid repo name: "${name}"`) }
-    await gt.newrepo(name)
-    await ndslt.update_repo()
-    await ndslt.update_node()
-    if (!save.node) {
-      const node = (await gt.readnodes(await gt.getrepoid(name)))[0]
-      save.node = node
-      await fllst.update_file(node)
-    }
-  }
-  $.change_node = async (node) => {
-    await check_file_save_dialog()
+$.single_input_message = f => async () => {
+  openmessage()
+  const ctn = dom({ class: "window" }, messagectn)
+  const ipt = dom({ tag: "input" }, ctn)
+  const btn = dom({ tag: "button", child: "Enter" }, ctn)
+  const apply = commit_dialog(f)
+  ipt.addEventListener("keydown", e => e.key === "Enter" ? apply(ipt) : 0)
+  btn.addEventListener("click", () => apply(ipt))
+}
+// 新建一个repo，当没有处于一个版本的时候，切换到这个新repo的根节点
+$.new_repo = async (name) => {
+  if (!name) { panic(`invalid repo name: "${name}"`) }
+  await gt.newrepo(name)
+  await ndslt.update_repo()
+  await ndslt.update_node()
+  if (!save.node) {
+    const node = (await gt.readnodes(await gt.getrepoid(name)))[0]
     save.node = node
     await fllst.update_file(node)
-    if (get_current_path()) {
-      await open_file({ path: get_current_path() })
-    } else {
-      editor.close()
+  }
+}
+$.change_node = async (node) => {
+  await check_file_save_dialog()
+  save.node = node
+  await fllst.update_file(node)
+  if (get_current_path()) {
+    await open_file({ path: get_current_path() })
+  } else {
+    editor.close()
+  }
+}
+$.new_file = async v => {
+  await gt.write(save.node, v, "")
+  await fllst.update_file(save.node)
+  fllst.highlight(get_current_path())
+}
+$.current_file_changed = false
+$.language_setting = { js: "javascript" }
+$.save_file = async content => {
+  const p = get_current_path()
+  await gt.write(save.node, p, content, "file", true)
+  $.current_file_changed = false
+  fllst.set_modified(p, false)
+}
+$.open_file = async ({ path }) => {
+  editor.open()
+  try {
+    set_current_path(path)
+    const v = await gt.read(save.node, path)
+    const ext = path.match(/\.([^\.]+)$/)?.[1] ?? ""
+    editor.value = ""
+    editor.change_language(language_setting[ext] ?? "plaintext")
+    editor.value = v
+    $.current_file_changed = false
+    fllst.highlight(path)
+    fllst.set_modified(path, false)
+  } catch (e) {
+    editor.close()
+    throw e
+  }
+}
+$.get_current_path = () => save[save.node + "/file"]
+$.set_current_path = v => save[save.node + "/file"] = v
+$.new_node_dialog = () => {
+  openmessage()
+  const ctn = dom({ class: "window" }, messagectn)
+  const warning = "This action will lock current version," +
+    " and you will no longer being able to edit it, proceed?"
+  dom({ child: warning }, ctn)
+  const btn = dom({ tag: "button", child: "Yes" }, ctn)
+  btn.onclick = async () => {
+    try {
+      const newnode = await gt.newnode(save.node)
+      await change_node(newnode)
+      await ndslt.update_repo()
+      await ndslt.change_repo_by_node(save.node)
+    } finally {
+      closemessage()
     }
   }
-  $.new_file = async v => {
-    await gt.write(save.node, v, "")
+}
+$.commit_dialog = f => async (...a) => {
+  try { await f(...a); closemessage() } catch (e) {
+    openmessage()
+    const child = e instanceof Error ? (e.message + "\n" + e.stack) : e
+    messagectn.append(dom({ tag: "pre", class: "error", child }))
+    throw e
+  }
+}
+$.rename_ref_dialog = (new_ref = true) => async (v = {}) => {
+  openmessage()
+  const ctn = dom({ class: "window" }, messagectn)
+  const ns = dom({ class: "container v-ctn" }, ctn)
+  const ndslt = create_node_selector(ns)
+  ndslt.nodesvg.style.height = "500px"
+  await ndslt.update_repo()
+  await ndslt.update_node()
+  const ipt = dom({ tag: "input" }, ctn)
+  const btn = dom({ tag: "button", child: "Enter" }, ctn)
+  if (v.content && v.path) {
+    ipt.value = v.path
+    await ndslt.change_repo_by_node(v.content)
+  }
+  const apply = commit_dialog(async () => {
+    const n = ipt.value
+    const i = ndslt.current_node
+    if (n && i) {
+      if (new_ref) { await gt.write(save.node, n, i, "ref") }
+      else if (v.path) {
+        await gt.remove(save.node, v.path)
+        await gt.write(save.node, n, i, "ref")
+      }
+    }
+    else { panic(`new ref failed because of invalid argument`) }
     await fllst.update_file(save.node)
     fllst.highlight(get_current_path())
-  }
-  $.open_file = async ({ path }) => {
-    editor.open()
-    try {
-      set_current_path(path)
-      const v = await gt.read(save.node, path)
-      const ext = path.match(/\.([^\.]+)$/)?.[1] ?? ""
-      editor.value = ""
-      editor.change_language(language_setting[ext] ?? "plaintext")
-      editor.value = v
-      $.current_file_changed = false
-      fllst.highlight(path)
-      fllst.set_modified(path, false)
-    } catch (e) {
-      editor.close()
-      throw e
-    }
-  }
-  $.get_current_path = () => save[save.node + "/file"]
-  $.set_current_path = v => save[save.node + "/file"] = v
-  $.new_node_dialog = () => {
-    openmessage()
-    const ctn = dom({ class: "window" }, messagectn)
-    const warning = "This action will lock current version," +
-      " and you will no longer being able to edit it, proceed?"
-    dom({ child: warning }, ctn)
-    const btn = dom({ tag: "button", child: "Yes" }, ctn)
-    btn.onclick = async () => {
-      try {
-        const newnode = await gt.newnode(save.node)
-        await change_node(newnode)
-        await ndslt.update_repo()
-        await ndslt.change_repo_by_node(save.node)
-      } finally {
-        closemessage()
-      }
-    }
-  }
-  $.commit_dialog = f => async (...a) => {
-    try { await f(...a); closemessage() } catch (e) {
-      log(e)
+  })
+  ipt.addEventListener("keydown", e => e.key === "Enter" ? apply() : 0)
+  btn.addEventListener("click", apply)
+}
+$.check_file_save_dialog = async () => {
+  if (current_file_changed) {
+    await new Promise(acc => {
       openmessage()
-      messagectn.append(dom({
-        tag: "pre", class: "error",
-        child: e.message + "\n" + e.stack
-      }))
-      throw e
-    }
-  }
-  $.rename_ref_dialog = (new_ref = true) => async (v = {}) => {
-    openmessage()
-    const ctn = dom({ class: "window" }, messagectn)
-    const ns = dom({ class: "container v-ctn" }, ctn)
-    const ndslt = create_node_selector(ns)
-    ndslt.nodesvg.style.height = "500px"
-    await ndslt.update_repo()
-    await ndslt.update_node()
-    const ipt = dom({ tag: "input" }, ctn)
-    const btn = dom({ tag: "button", child: "Enter" }, ctn)
-    if (v.content && v.path) {
-      ipt.value = v.path
-      await ndslt.change_repo_by_node(v.content)
-    }
-    const apply = commit_dialog(async () => {
-      const n = ipt.value
-      const i = ndslt.current_node
-      if (n && i) {
-        if (new_ref) { await gt.write(save.node, n, i, "ref") }
-        else if (v.path) {
-          await gt.remove(save.node, v.path)
-          await gt.write(save.node, n, i, "ref")
-        }
-      }
-      else { panic(`new ref failed because of invalid argument`) }
-      await fllst.update_file(save.node)
-      fllst.highlight(get_current_path())
+      const ctn = dom({ class: "window" }, messagectn)
+      dom({ child: "File has been changed, do you want to save it?" }, ctn)
+      const btnyes = dom({ tag: "button", child: "Yes" }, ctn)
+      const btndis = dom({ tag: "button", child: "Discard" }, ctn)
+      const btnno = dom({ tag: "button", child: "No" }, ctn)
+      btnyes.onclick = commit_dialog(async () => (
+        await save_file(editor.value), acc()))
+      btndis.onclick = commit_dialog(() =>
+        (fllst.set_modified(get_current_path(), false), acc()))
+      btnno.onclick = () => closemessage()
     })
-    ipt.addEventListener("keydown", e => e.key === "Enter" ? apply() : 0)
-    btn.addEventListener("click", apply)
   }
-  // 有没有正在编辑的文件？如果有那么弹出对话框询问用户是否保存
-  $.check_file_save_dialog = async () => {
-    if (current_file_changed) {
-      await new Promise(acc => {
-        openmessage()
-        const ctn = dom({ class: "window" }, messagectn)
-        dom({ child: "File has been changed, do you want to save it?" }, ctn)
-        const btnyes = dom({ tag: "button", child: "Yes" }, ctn)
-        const btndis = dom({ tag: "button", child: "Discard" }, ctn)
-        const btnno = dom({ tag: "button", child: "No" }, ctn)
-        btnyes.onclick = commit_dialog(async () => (
-          await save_file(editor.value), acc()))
-        btndis.onclick = commit_dialog(() =>
-          (fllst.set_modified(get_current_path(), false), acc()))
-        btnno.onclick = () => closemessage()
-      })
-    }
-  }
+}
+$.simple_rename_dialog = (v, f) => {
+  openmessage()
+  const ctn = dom({ class: "window" }, messagectn)
+  const ipt = dom({ tag: "input", value: v }, ctn)
+  const btn = dom({ tag: "button", child: "Enter" }, ctn)
+  const apply = commit_dialog(f)
+  ipt.addEventListener("keydown", e => e.key === "Enter" ? apply(v, ipt.value) : 0)
+  btn.addEventListener("click", () => apply(v, ipt.value))
+}
+$.rename_file_dialog = v => simple_rename_dialog(v.path, async (o, n) => {
+  await gt.rename(save.node, o, n)
+  await fllst.update_file(save.node)
+  await open_file({ path: n })
+})
+$.rename_repo_dialog = async () => simple_rename_dialog(
+  await gt.getnoderepo(save.node), async (o, n) => {
+    await gt.renamerepo(o, n)
+    await ndslt.update_repo()
+  })
+$.execjs = (a, b) => gt.read_relative(a, b, async v => await sdbx.exec(v, { execjs }))
+$.exec_dialog = commit_dialog(async v => {
+  if (current_file_changed && v.path === get_current_path()) { await save_file(editor.value) }
+  save.last_exec = v; sdbx.stop(); await execjs(v.path, v.node)
+})
+$.opencontextmenu = (x, y) => {
+  contextmenuctn.style.left = x - 1 + "px"
+  contextmenuctn.style.top = y - 1 + "px"
+  contextmenuctn.style.display = ""
+  contextmenuctn.innerHTML = ""
+}
+$.closecontextmenu = () => { contextmenuctn.style.display = "none" }
+$.openmessage = () => {
+  messagectn.style.display = ""
+  messagectn.innerHTML = ""
+}
+$.closemessage = () => { messagectn.style.display = "none" }
 
+window.addEventListener("beforeunload", (e) => {
+  if (!$.current_file_changed) { return }
+  check_file_save_dialog()
+  return e.returnValue = "file has been changed"
+})
+window.addEventListener("load", async () => {
+  // DOM 结构
   $.topctn = dom({ class: "container", style: { userSelect: "none" } }, document.body)
   $.sidectn = dom({ class: "container v-ctn", style: { width: "150px" } }, topctn)
   $.mainctn = dom({ class: "container v-ctn", style: { width: "700px" } }, topctn)
@@ -270,59 +305,41 @@ window.addEventListener("load", async () => {
   $.sdbx = sandbox(evalctn)
   $.textctn = dom({ class: "container" }, mainctn)
   $.editor = create_editor(textctn)
-  editor.close()
-
-  $.opencontextmenu = (x, y) => {
-    contextmenuctn.style.left = x - 1 + "px"
-    contextmenuctn.style.top = y - 1 + "px"
-    contextmenuctn.style.display = ""
-    contextmenuctn.innerHTML = ""
-  }
-  $.closecontextmenu = () => { contextmenuctn.style.display = "none" }
   $.contextmenuctn = dom({
     class: "context-menu", style: { display: "none" },
     onpointerleave: closecontextmenu
   }, document.body)
-
   $.messagectn = dom({
     class: "container message-background", style: { display: "none" },
     onclick: e => { if (e.target === messagectn) { closemessage() } }
   }, document.body)
-  $.openmessage = () => {
-    messagectn.style.display = ""
-    messagectn.innerHTML = ""
-  }
-  $.closemessage = () => { messagectn.style.display = "none" }
-
   $.newrepobtn = dom({ tag: "button", child: "newrepo" }, sidectn)
+  $.renamerepobtn = dom({ tag: "button", child: "renamerepo" }, sidectn)
+  $.delrepobtn = dom({ tag: "button", child: "delrepo" }, sidectn)
+  $.newnodebtn = dom({ tag: "button", child: "newnode" }, sidectn)
+  $.newfilebtn = dom({ tag: "button", child: "newfile" }, sidectn)
+  $.newrefbtn = dom({ tag: "button", child: "newref" }, sidectn)
+  $.filectn = dom({ class: "container v-ctn" }, sidectn)
+  filectn.style.paddingTop = "10px"
+  $.fllst = create_file_list(filectn)
+  $.nodectn = dom({ class: "container v-ctn" }, sidectn)
+  $.ndslt = create_node_selector(nodectn)
+  $.description = dom({ tag: "textarea" }, nodectn)
+  description.style.height = "30%"
+
+  // 事件绑定
   newrepobtn.addEventListener("click",
     single_input_message(ipt => new_repo(ipt.value)))
-  $.newnodebtn = dom({ tag: "button", child: "newnode" }, sidectn)
+  renamerepobtn.addEventListener("click", rename_repo_dialog)
   newnodebtn.addEventListener("click", new_node_dialog)
-
-  $.newfilebtn = dom({ tag: "button", child: "newfile" }, sidectn)
   newfilebtn.addEventListener("click",
     single_input_message(ipt => new_file(ipt.value)))
-  $.newrefbtn = dom({ tag: "button", child: "newref" }, sidectn)
   newrefbtn.addEventListener("click", rename_ref_dialog())
-
-  $.current_file_changed = false
   editor.on("change", () => {
     current_file_changed = true
     fllst.set_modified(get_current_path(), true)
   })
-  $.save_file = async content => {
-    const p = get_current_path()
-    await gt.write(save.node, p, content, "file", true)
-    $.current_file_changed = false
-    fllst.set_modified(p, false)
-  }
-  $.language_setting = { js: "javascript" }
-
-  $.filectn = dom({ class: "container v-ctn" }, sidectn)
-  filectn.style.paddingTop = "10px"
-  $.fllst = create_file_list(filectn)
-  fllst.update_file(save.node)
+  ndslt.on("nodeselect", change_node)
   fllst.on("fileselect", async v => {
     await check_file_save_dialog()
     await open_file(v)
@@ -330,20 +347,6 @@ window.addEventListener("load", async () => {
   const _rrdf = rename_ref_dialog(false)
   fllst.on("refselect", _rrdf)
   editor.on("filesave", commit_dialog(save_file))
-  $.rename_file_dialog = v => {
-    openmessage()
-    const ctn = dom({ class: "window" }, messagectn)
-    const ipt = dom({ tag: "input", value: v.path }, ctn)
-    const btn = dom({ tag: "button", child: "Enter" }, ctn)
-    const apply = commit_dialog(async () => {
-      const oldname = v.path, newname = ipt.value
-      await gt.rename(save.node, oldname, newname)
-      await fllst.update_file(save.node)
-      await open_file({ path: newname })
-    })
-    ipt.addEventListener("keydown", e => e.key === "Enter" ? apply() : 0)
-    btn.addEventListener("click", apply)
-  }
   fllst.on("rename", v => {
     if (v.mode === "ref") { _rrdf(v) }
     else { rename_file_dialog(v) }
@@ -354,11 +357,6 @@ window.addEventListener("load", async () => {
     if (get_current_path() === v.path) {
       editor.close()
     }
-  })
-  $.execjs = (a, b) => gt.read_relative(a, b, async v => await sdbx.exec(v, { execjs }))
-  $.exec_dialog = commit_dialog(async v => {
-    if (current_file_changed && v.path === get_current_path()) { await save_file(editor.value) }
-    save.last_exec = v; sdbx.stop(); await execjs(v.path, v.node)
   })
   fllst.on("execute", exec_dialog)
   window.addEventListener("keydown", e => {
@@ -374,14 +372,11 @@ window.addEventListener("load", async () => {
     }
   })
 
-  $.nodectn = dom({ class: "container v-ctn" }, sidectn)
-  $.ndslt = create_node_selector(nodectn)
-  ndslt.on("nodeselect", change_node)
-
-  $.description = dom({ tag: "textarea" }, nodectn)
-  description.style.height = "30%"
-
   // 初始化
+  $.save = db.saveobj("6s80s1u052ftdj009g2fd7brflbv1iq8")
+  await save.init
+  fllst.update_file(save.node)
+  editor.close()
   await ndslt.update_repo()
   if (save.node) { await ndslt.change_repo_by_node(save.node) }
   if (get_current_path()) { await open_file({ path: get_current_path() }) }
